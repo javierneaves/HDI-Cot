5c83bf1da7e563f6913c5e815ae7dd2e302aa6f4
Neaves Voltio <neaves@voltio.us>
2023-10-28 10:55:08 -0600

    add react hook validation

--- src/components/FormName.jsx
+++ src/components/FormName.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const FormName = () => {
+  const { register, handleSubmit, formState } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="container text-center mt-5">
@@ -10,34 +16,63 @@ const FormName = () => {
         </h5>
       </div>
       <div className="container mt-5">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-3">
-            <label htmlFor="formNombre" className="form-label fw-bold text-body-secondary">
+            <label
+              htmlFor="nombre"
+              className="form-label fw-bold text-body-secondary"
+            >
               Nombre
             </label>
             <input
               type="text"
               className="form-control border-secondary "
-              id="formNombre"
+              name="nombre"
               placeholder="Escribe tu nombre"
+              {...register("nombre", { required: true })}
             />
+            {formState.errors.nombre && (
+              <div className="text-danger" >
+                Este campo es obligatorio
+              </div>
+            )}
           </div>
           <div className="mb-3">
-            <label htmlFor="formCp" className="form-label fw-bold text-body-secondary">
+            <label
+              htmlFor="codigoPostal"
+              className="form-label fw-bold text-body-secondary"
+            >
               Código Postal
             </label>
             <input
               type="text"
               className="form-control border-secondary"
-              id="formCp"
+              name="codigoPostal"
               placeholder="Escribe tu CP"
+              {...register("codigoPostal", {
+                required: true,
+                pattern: /^[0-9]{5}$/,
+              })}
             />
+            {formState.errors.codigoPostal &&
+              formState.errors.codigoPostal.type === "required" && (
+                <div className="text-danger" >
+                  Este campo es obligatorio
+                </div>
+              )}
+            {formState.errors.codigoPostal &&
+              formState.errors.codigoPostal.type === "pattern" && (
+                <div className="text-danger" >
+                  Por favor, ingresa un código postal válido de 5 números
+                </div>
+              )}
+          </div>
+          <div className="container p-5 text-end">
+            <button type="submit" className="btn btn-primary btn-lg">
+              Siguiente{" "}
+            </button>
           </div>
         </form>
-        <div className="container p-5 text-end">
-          <button type="button" className="btn btn-primary btn-lg">Siguiente </button>
-        </div>
-                  
       </div>
     </>
   );
